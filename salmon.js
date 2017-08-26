17cd0cbcc5a468e8bcf524cb88fcbcd740abc1b3
oneEyedBunny <allyson7579@yahoo.com>
2017-08-26 16:29:21 -0700

    completed js code for assignment

--- salmon.js
+++ salmon.js
@@ -42,34 +42,33 @@ var hoursInDay = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 //function to build out the actual list of hours/cookies
 function buildTable () {
-  var tableBody = document.getElementById("cookieTable");//connecting to HTML doc
+  var tableBody = document.getElementById("cookieTable")//place in HTML doc that this goes
   var rowStart = document.createElement("tr"); //initial row in html
   var hours = document.createElement("th"); //initial header
   hours.innerText = "Store Locations";
-  rowStart.appendChild(hours); //header is blank
+  rowStart.appendChild(hours);
   tableBody.appendChild(rowStart);
 
-    for (l = 0; l < locations.length; l++){ //loops through location array
-     var colStart = document.createElement("tr");
-     var storeLocations = document.createElement("th");
-     storeLocations.innerText = locations[l].name;
-     colStart.appendChild(storeLocations)
+  for (l = 0; l < locations.length; l++){ //loops through LOCATION array
+   var colStart = document.createElement("tr");
+   var storeLocations = document.createElement("th");
+   storeLocations.innerText = locations[l].name;
+   colStart.appendChild(storeLocations)
 
-    for (h = 0; h < hoursInDay.length; h++){// loops through hours array
-      if(l == 0){
-        hours = document.createElement("th");
-        hours.innerText = hoursInDay[h];
-        rowStart.appendChild(hours);
-      }//closed if statement
-      var cookieData= document.createElement("td");
-      console.log(cookieData);
-      var cookies = locations[l].cookiesPurchPerHour();
-      cookieData.innerText = cookies;
-      colStart.appendChild(cookieData);
-      tableBody.appendChild(colStart); //puts location and cookie data in table
+  for (h = 0; h < hoursInDay.length; h++){// loops through HOURS array
+    if(l == 0){                           //created the hours header only 1x when the location position is at the 0 index
+      hours = document.createElement("th");
+      hours.innerText = hoursInDay[h];
+      rowStart.appendChild(hours);
+    }//closed if statement
+    var cookieData= document.createElement("td");
+    var cookies = locations[l].cookiesPurchPerHour();
+    cookieData.innerText = cookies;
+    colStart.appendChild(cookieData); //puts cookieData in the cells
+    tableBody.appendChild(colStart); //puts location and cookie data in table in the HTML doc
 
-      locations[l].totalCookiesPerLocation += cookies; //each hour loop it tallies the cookieData
-      console.log(locations[l].totalCookiesPerLocation)
+    locations[l].totalCookiesPerLocation += cookies; //each hour loop it tallies the cookieData by location
+
     }//closed inner for loop on hours
 
      if (l == 0){ //runs inside of location loop
@@ -82,7 +81,7 @@ function buildTable () {
      grandTotalData.innerText = numberWithCommas(locations[l].totalCookiesPerLocation); //fill the data with the total cookies by location
      colStart.appendChild(grandTotalData);
      tableBody.appendChild(colStart);
-  }//closed for loop on locations
+   }//closed for loop on locations
 }//closes buildTable function
 
 buildTable();
